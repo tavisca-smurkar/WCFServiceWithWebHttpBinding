cad15bd48f8199e23ef2ea08fdca7a6506304a0b
tavisca-smurkar <smurkar@tavisca.com>
2016-09-07 18:16:48 +0530

    Initial commit

---
+++ WCFServiceWithWebHttpBindingHost/WCFServiceWithWebHttpBindingHost/HelloService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServiceWithWebHttpBindingHost
+{
+    public class HelloService : IHelloService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello {name}";
+        }
+    }
+}
---
+++ WCFServiceWithWebHttpBindingHost/WCFServiceWithWebHttpBindingHost/IHelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+namespace WCFServiceWithWebHttpBindingHost
+{
+    [ServiceContract]
+    public interface IHelloService
+    {
+        [OperationContract]
+        [WebInvoke(ResponseFormat =WebMessageFormat.Json , UriTemplate = "/{name}")]
+        string GetMessage(string name);
+    }
+}
