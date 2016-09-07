5e65548477f6144463cc65cfbfad0e6b00489cfc
tavisca-smurkar <smurkar@tavisca.com>
2016-09-07 18:38:12 +0530

    Service Host Created

---
+++ WCFServiceWithWebHttpBindingHost/ServiceHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+using WCFServiceWithWebHttpBindingHost;
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ( ServiceHost host = new ServiceHost(typeof(HelloService)))
+            {
+                host.Open();
+                Console.WriteLine("Service is started at {0}", DateTime.Now);
+                Console.ReadLine();
+            }
+        }
+    }
+}
